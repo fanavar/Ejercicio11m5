0e665a085bedcd9b2b4c4fa46543c089a217c8d8
Fabián <fanavar@alumnos.ubiobio.cl>
2023-07-13 19:54:59 -0400

    ejercicio 11

--- app/src/main/java/cl/awakelab/ejercicio11/MainActivity.kt
+++ app/src/main/java/cl/awakelab/ejercicio11/MainActivity.kt
@@ -32,13 +32,12 @@ Ejercicio C
 }
 
  */
-
+Ejercicio 11 letra a
 fun main() {
     val amanda = Person("Amanda", 33, "play tennis", null)
     val atiqah = Person("Atiqah", 28, "climb", amanda)
     amanda.showProfile()
     atiqah.showProfile()
-
 }
 class Person(val name: String, val age: Int, val hobby: String?, val referrer: Person?) {
     fun showProfile() {
@@ -49,7 +48,7 @@ class Person(val name: String, val age: Int, val hobby: String?, val referrer: P
             println("Doesn't have a referrer")
         }
         else{
-            println("has a referrer ${referrer.name}")
+            println("has a referrer ${referrer.name} who likes to play ${referrer.hobby}")
         }
     }
 }
